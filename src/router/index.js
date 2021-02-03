5817970e6be6f5fdd2f3c48eea64ad8dcb7ae92c
albukzek <albukzek@gmail.com>
2021-02-04 00:51:22 +0300

    All Layout

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import Categories from '../views/Categories.vue'
+import Detail from '../views/Detail.vue'
+import Hystory from '../views/Hystory.vue'
+import Planning from '../views/Planning.vue'
+import Profile from '../views/Profile.vue'
+import Record from '../views/Record.vue'
+import Register from '../views/Register.vue'
 
 Vue.use(VueRouter)
 
@@ -11,14 +19,60 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/login',
+    name: 'login',
+    meta: {layout: 'empty'},
+    component: Login
+  },
+  {
+    path: '/categories',
+    name: 'categories',
+    meta: {layout: 'main'},
+    component: Categories
   }
-]
+  ,
+  {
+    path: '/detail',
+    name: 'detail',
+    meta: {layout: 'main'},
+    component: Detail
+  }
+  ,
+  {
+    path: '/hystory',
+    name: 'hystory',
+    meta: {layout: 'main'},
+    component: Hystory
+  }
+  ,
+  {
+    path: '/planning',
+    name: 'planning',
+    meta: {layout: 'main'},
+    component: Planning
+  }
+  ,
+  {
+    path: '/profile',
+    name: 'profile',
+    meta: {layout: 'main'},
+    component: Profile
+  }
+  ,
+  {
+    path: '/record',
+    name: 'record',
+    meta: {layout: 'main'},
+    component: Record
+  }
+  ,
+  {
+    path: '/register',
+    name: 'register',
+    meta: {layout: 'empty'},
+    component: Register
+  }
+]  
 
 const router = new VueRouter({
   mode: 'history',
